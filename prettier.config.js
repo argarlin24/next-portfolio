d90b6a5e1a2fbe90398004edcd0f7b78bb2c282a
Adam Garling <33332581+argarlin24@users.noreply.github.com>
2024-04-30 22:04:44 -0700

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,33 @@
+/** @type {import("@trivago/prettier-plugin-sort-imports").PrettierConfig} */
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  proseWrap: 'always',
+  importOrderSeparation: true,
+  importOrderCaseInsensitive: true,
+  importOrder: [
+    '^(react/(.*)$)|^(react$)|^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '^assets/(.*)$',
+    '^graphql/(.*)$',
+    '^molecules/(.*)$',
+    '^components/(.*)$',
+    '^templates/(.*)$',
+    '^utils/(.*)$',
+    '^theme/(.*)$',
+    '^types/(.*)$',
+    '^[./]',
+  ],
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
+
+module.exports = config;
