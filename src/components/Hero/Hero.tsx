58bd0d478410b33a3063493288f97a81dc05ac15
Adam Garling <argarlin@gmail.com>
2026-08-06 20:20:00 -0700

    lower line count

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,8 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import React from 'react';
 import type { FC } from 'react';
-
-import dynamic from 'next/dynamic';
 
 import { ComponentHeroRecord } from '@/graphql/generated';
 
