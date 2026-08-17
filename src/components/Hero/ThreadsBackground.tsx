a6f7f7221120267bbba34c9b6f4488acee33706a
Adam Garling <argarlin@gmail.com>
2026-08-16 20:52:22 -0700

    testing threads

--- src/components/Hero/ThreadsBackground.tsx
+++ src/components/Hero/ThreadsBackground.tsx
@@ -16,6 +16,8 @@ const ThreadsBackground = ({
   amplitude = 1.5,
   distance = 0.3,
 }: ThreadsBackgroundProps) => {
+  // Temporarily disabled for testing
+  return null;
   return (
     <div className="pointer-events-none absolute -bottom-1/2 -top-1/2 -left-20 right-0 -rotate-6 origin-left w-[120%]">
       <Threads amplitude={amplitude} distance={distance} />
